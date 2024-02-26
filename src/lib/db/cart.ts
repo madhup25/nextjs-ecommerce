c3930358a488635eab563153ef9a21af30b675d1
Madhup Mamodia <mamodia.madhup@gmail.com>
2024-02-27 00:58:22 +0530

    Added cart page and cart functionality

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -3,16 +3,20 @@ import { prisma } from "./prisma";
 import { Cart, Prisma } from "@prisma/client";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
-    include: { items: { include: { product: true } } }
-}>
+    include: { items: { include: { product: true; }; }; };
+}>;
+
+export type CartItemWithProduct = Prisma.CartItemGetPayload<{
+    include: { product: true; };
+}>;
 
 export type ShoppingCart = CartWithProducts & {
     size: number,
-    subtotal: number
-}
+    subtotal: number;
+};
 
 export async function getCart(): Promise<ShoppingCart | null> {
-    const localCartId = cookies().get("localCartId")?.value
+    const localCartId = cookies().get("localCartId")?.value;
     const cart = localCartId ?
         await prisma.cart.findUnique({
             where: { id: localCartId },
@@ -21,26 +25,26 @@ export async function getCart(): Promise<ShoppingCart | null> {
         :
         null;
 
-    if (!cart) return null
+    if (!cart) return null;
     return {
         ...cart,
         size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
         subtotal: cart.items.reduce((acc, item) => acc + item.quantity * item.product.price, 0)
-    }
+    };
 }
 
 export async function createCart(): Promise<ShoppingCart> {
     const newCart = await prisma.cart.create({
         data: {}
-    })
+    });
 
     // Note: Needs encryption + secure settings in real production app
-    cookies().set("localCartId", newCart.id)
+    cookies().set("localCartId", newCart.id);
 
     return {
         ...newCart,
         items: [],
         size: 0,
         subtotal: 0
-    }
+    };
 }
